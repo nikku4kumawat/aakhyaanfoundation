8406ba6b5ab95656919bd7107c55257da45844c6
nikku4kumawat <nikkukumawat4@gmail.com>
2026-06-30 18:30:58 +0530

    Updated website

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const BASE_URL = "https://akhyanfoundationback.onrender.com";
 const API_PATHS = {
   ACTIVITY_CALENDAR: `${BASE_URL}/api/activity-calendar`,
   PRESS_COVERAGE: `${BASE_URL}/api/press-coverage`,
-  ADMIN_BLOG: `${BASE_URL}/api/admin-blog`,
+  ADMIN_BLOG: `${BASE_URL}/api/blog`,
 };
